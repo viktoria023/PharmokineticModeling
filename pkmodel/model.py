533601fe7a6388370997465a6a9fc3d19a130fb9
viktoria023 <vbrunner@student.ethz.ch>
2021-10-20 15:18:42 +0100

    refined model class

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -24,22 +24,22 @@ class Model:
     # this stuff should come from the input to the class
 
     # simple intravenous model as from prototype.py
-    def dose(t, X):
-        return X
+    def dose(t, self):
+        return self.X
 
-    def ivModel(t, y, Q_p1, V_c, V_p1, CL, X):
+    def ivModel(t, y, self):
         q_c, q_p1 = y
-        transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-        dqc_dt = dose(t, X) - q_c / V_c * CL - transition
+        transition = self.Q_p1 * (q_c / self.V_c - q_p1 / self.V_p1)
+        dqc_dt = dose(t, self.X) - q_c / self.V_c * self.CL - transition
         dqp1_dt = transition
         return [dqc_dt, dqp1_dt]
 
     # subcutaneous model
-    def scModel(t, y, Q_p1, V_c, V_p1, CL, X, k_a):
+    def scModel(t, y, self):
         q_c, q_p1, q_0 = y
-        transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-        dq0_dt = dose(t, X) - k_a*q_0
-        dqc_dt = k_a*q_0 - q_c / V_c * CL - transition
+        transition = self.Q_p1 * (q_c / self.V_c - q_p1 / self.V_p1)
+        dq0_dt = dose(t, self.X) - self.k_a*q_0
+        dqc_dt = self.k_a*q_0 - q_c / self.V_c * self.CL - transition
         dqp1_dt = transition
         return [dqc_dt, dqp1_dt, dq0_dt]
 
