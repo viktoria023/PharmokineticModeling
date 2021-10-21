4c5e27a178a19f2163c6c372c96cc50ae0eea161
jeanneinchauspe <jeanne.inchauspe@gmail.com>
2021-10-21 12:26:36 +0100

    first modifications of test_model.py

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -1,15 +1,24 @@
 import unittest
-import pkmodel as pk
-
+from model import Model
 
 class ModelTest(unittest.TestCase):
     """
     Tests the :class:`Model` class.
     """
-    def test_create(self):
+    def test_model_iv_1(self):
         """
-        Tests Model creation.
+        Tests Dosage creation.
         """
-        model = pk.Model()
-        self.assertEqual(model.value, 42)
+        model1 = Model(self, value, Q_p1=1.0, V_c=1.0, V_p1=1.0, CL=1.0, X=1.0, k_a=None)
+        self.assertEqual(model1.dose, 42)
+        self.assertEqual(model1.ivModel, 42)
 
+    def test_model_sc_1(self):
+        """
+        Tests Dosage creation.
+        """
+        model1 = Model(self, value, Q_p1=1.0, V_c=1.0, V_p1=1.0, CL=1.0, X=1.0, k_a=None)
+        self.assertEqual(model1.dose, 42)
+
+        self.assertEqual(model1.scModel, 42)
+    
