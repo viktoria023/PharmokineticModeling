7bffa356b57bed8dfc3e346b7c233a54f58a9210
viktoria023 <vbrunner@student.ethz.ch>
2021-10-22 10:56:17 +0100

    Adjusted docstrings to fit sphinx format

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -3,21 +3,21 @@
 #
 
 class Model:
-    """A Pharmokinetic (PK) model
+    """A Pharmokinetic (PK) model.
 
     Parameters
     ----------
 
-    value:  numeric, an example parameter to test if class can be called correctly
-    Q_p1:   [mL/h] float, the transition rate between central compartment and peripheral compartment 1
-    V_c:    [mL] float, the volume of the central compartment
-    V_p1:   [mL] float, the volume of the first peripheral compartment
-    CL:     [mL/h] float, the clearance/elimination rate from the central compartment
-    X:      [ng] float, the amount of drug administered per injection
-    k_a:     [/h] float is the absorption rate, only used for the sc dosing
-    scheme: ['clt','hlt','dt'] pattern of dose administration as specified by user. clt = instantaneous limited time, hlt = hourly limited time, dt = defined time points
-    stop:   [h] float, timepoint at which injection stops. Only used in 'ilt' and 'hlt' scheme
-    tps:    [h] float, timepoints of dose administration as specified by the user. Only used in 'dt' scheme
+    :param value:  numeric, an example parameter to test if class can be called correctly
+    :param Q_p1:   [mL/h] float, the transition rate between central compartment and peripheral compartment 1
+    :param V_c:    [mL] float, the volume of the central compartment
+    :param V_p1:   [mL] float, the volume of the first peripheral compartment
+    :param CL:     [mL/h] float, the clearance/elimination rate from the central compartment
+    :param X:      [ng] float, the amount of drug administered per injection
+    :param k_a:     [/h] float is the absorption rate, only used for the sc dosing
+    :param scheme: ['clt','hlt','dt'] pattern of dose administration as specified by user. clt = instantaneous limited time, hlt = hourly limited time, dt = defined time points
+    :param stop:   [h] float, timepoint at which injection stops. Only used in 'ilt' and 'hlt' scheme
+    :param tps:    [h] float, timepoints of dose administration as specified by the user. Only used in 'dt' scheme
     """
     def __init__(self, value, Q_p1=1.0, V_c=1.0, V_p1=1.0, CL=1.0, X=1.0, k_a=None, stop=None, scheme=None, tps=None):
         self.value = value
@@ -32,13 +32,13 @@ class Model:
         self.tps=tps
 
     def dose(t, self):
-        """Definition of dosing scheme
-        Depending on the defined dosing scheme, the function determines the released dosage X
-        at the  specified timepoint. 
+        """Definition of dosing scheme.
+
+        Depending on the defined dosing scheme, the function determines the released dosage X at the  specified timepoint. 
 
         Parameters
-        ___________
-        t: [h] float, timepoint tested by dose function
+        ----------
+        :param t: [h] float, timepoint tested by dose function
         
         :returns: float, amount of drug released at the specified timepoint t
         """
@@ -53,13 +53,14 @@ class Model:
         return 0
 
     def ivModel(t, y, self):
-        """Setting up intravenous model
+        """Setting up intravenous model.
+
         Sets up 2-compartment model 
 
         Parameters
-        ___________
-        t: [h] float, current timepoint 
-        y: [ng, ng] list, current amount of drug in the compartments
+        ----------
+        :param t: [h] float, current timepoint 
+        :param y: [ng, ng] list, current amount of drug in the compartments
 
         :returns: list with change of drug concentrations in respective compartments as a derivative of the time
         """
@@ -72,12 +73,13 @@ class Model:
     # subcutaneous model
     def scModel(t, y, self):
         """Setting up subcatenous model
+        
         Sets up 3-compartment model, with inital compartment giving off the drug to the central compartment at rate k_a
 
         Parameters
-        ___________
-        t: [h] float, current timepoint 
-        y: [ng, ng, ng] list, current amount of drug in the compartments
+        ----------
+        :param t: [h] float, current timepoint 
+        :param y: [ng, ng, ng] list, current amount of drug in the compartments
 
         :returns: list with change of drug concentrations in respective compartments as a derivative of the time
         """
--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -3,32 +3,32 @@
 #
 import numpy as np
 import scipy
+#from model import *
+import matplotlib.pylab as plt
 
 class Solution:
-    """A Pharmokinetic (PK) model solution
+    """A Pharmokinetic (PK) model solution.
 
     Parameters
     ----------
 
-    value: numeric, optional
-        an example paramter
-
+    :param value:  numeric, an example parameter to test if class can be called correctly
     """
     def __init__(self, value=44):
         self.value = value
 
-    def solver_iv(self, modelClass):
-        """A solver for the iv model
-        Takes the model and calculates the concentration changes as a function of the time
+    def solver_iv(self, modelClass):   
+        """A solver for the iv model.
+
+        Takes a specific initialization of the model and calculates the concentration time course.
 
         Parameters
-        __________
+        ----------
 
-        modelClass: class, an object of the type model. Defined in main file and 
-        has attribute .ivModel that takes t and y as input
+        :param modelClass: class, an object of the type Model. The specific instance of the object is initialized in the main file and passed to the function.
 
-        :returns: file with time and concentrations at given time for all compartments in file 'sol'
-        """
+        :returns: bunch SciPy.integrate_ivp object containing arrays t (timepoints) and y (concentration changes)
+        """ 
         t_eval = np.linspace(0, 1, 1000)
         y0 = np.array([0.0, 0.0])
 
@@ -40,8 +40,19 @@ class Solution:
         return sol
 
     def solver_sc(self, modelClass):
+        """A solver for the sc model.
+
+        Takes a specific initialization of the model and calculates the concentration time course.
+
+        Parameters
+        ----------
+
+        :param modelClass: class, an object of the type Model. The specific instance of the object is initialized in the main file and passed to the function.
+
+        :returns: bunch SciPy.integrate_ivp object containing arrays t (timepoints) and y (concentration changes)
+        """
         t_eval = np.linspace(0, 1, 1000)
-          y0 = np.array([0.0, 0.0])
+        y0 = np.array([0.0, 0.0])
 
         sol = scipy.integrate.solve_ivp(
         fun=lambda t, y: modelClass.scModel(t, y),
@@ -49,3 +60,29 @@ class Solution:
         y0=y0, t_eval=t_eval
         )
         return sol
+
+    #fig = plt.figure()
+    def plotter(self, sol, model_name, save="no"):
+        """A method to visualise the ode solutions using the matplotlib library.
+
+        :parameters: 
+                sol, required, object : the ode solution returned by solution.solver_sc or solver_iv : 
+                save, required, string, yes or no- if yes, plot saved in PNG
+                model_name, string, required : "iv" or "sc" 
+
+        :returns: 
+                a plot window or a an output file, output_plot.png
+        """
+        self.sol = sol
+        self.model_name = model_name
+        fig = plt.figure()
+        plt.plot(sol.t, sol.y[0, :], label = model_name + '- q_c')
+        plt.plot(sol.t, sol.y[1, :], label = model_name + '- q_p1')
+        plt.legend()
+        plt.ylabel('drug mass [ng]')
+        plt.xlabel('time [h]')
+        
+        if (save == "yes"):
+            plt.savefig("output_plot.png")
+        else:
+            plt.show()
