65705ed71107004fc6403e33d69d764d857d8946
jeanneinchauspe <jeanne.inchauspe@gmail.com>
2021-10-22 10:37:36 +0100

    all changes to test_model.py
    Signed-off-by: jeanneinchauspe <jeanne.inchauspe@gmail.com>

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -9,16 +9,17 @@ class ModelTest(unittest.TestCase):
         """
         Tests Dosage creation.
         """
-        model1 = Model(self, value, Q_p1=1.0, V_c=1.0, V_p1=1.0, CL=1.0, X=1.0, k_a=None)
+        model1 = Model(self, value = 42, Q_p1=1.0, V_c=1.0, V_p1=1.0, CL=1.0, X=1.0, k_a=None)
         self.assertEqual(model1.dose, 42)
         self.assertEqual(model1.ivModel, 42)
+        self.assertEqual(model1.value, 42)
 
     def test_model_sc_1(self):
         """
         Tests Dosage creation.
         """
-        model1 = Model(self, value, Q_p1=1.0, V_c=1.0, V_p1=1.0, CL=1.0, X=1.0, k_a=None)
+        model1 = Model(self, value = 42 , Q_p1=1.0, V_c=1.0, V_p1=1.0, CL=1.0, X=1.0, k_a=None)
         self.assertEqual(model1.dose, 42)
-
         self.assertEqual(model1.scModel, 42)
+        self.assertEqual(model1.value, 42)
     
